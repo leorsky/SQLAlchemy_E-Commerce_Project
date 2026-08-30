96842b21f48de0fa9490811537d9a818ffade525
LeoR <leorsky77@gmail.com>
2026-08-30 22:46:28 +0300

    Implement CRUD operations

--- main.py
+++ main.py
@@ -74,3 +74,92 @@ item3 = OrderItem(
 )
 
 Base.metadata.create_all(engine)
+
+with SessionLocal() as session:
+    try:
+        session.add_all([
+            user1,
+            user2,
+            category1,
+            category2,
+            product1,
+            product2,
+            product3,
+            order1,
+            order2,
+            item1,
+            item2,
+            item3,
+        ])
+        session.commit()
+
+        full_orm = [
+            user1,
+            user2,
+            category1,
+            category2,
+            product1,
+            product2,
+            product3,
+            order1,
+            order2,
+            item1,
+            item2,
+            item3,
+        ]
+
+        for el in full_orm:
+            session.refresh(el)
+
+        statement1 = select(Product).where(Product.id == 1)
+        result1 = session.execute(statement1)
+        product_1 = result1.scalar_one_or_none()
+
+        if product_1 is None:
+            raise Exception(f'Продукт не найден.')
+        else:
+            print(f'{product_1.name} - {product_1.price}')
+
+            for item in product_1.order_item:
+                print(item.order.id, item.order.status)
+
+        statement2 = select(Product)
+        result2 = session.execute(statement2)
+        products1 = result2.scalars()
+        for el in products1:
+            print(f'{el.name} - {el.price}\n')
+
+        statement3 = select(Product).where(Product.price > 900)
+        result3 = session.execute(statement3)
+        products2 = result3.scalars().all()
+
+        if not products2:
+            raise Exception(f'Продукт не найден.')
+        else:
+            for el in products2:
+                print(f'{el.name} - {el.price}\n')
+
+        statement4 = select(Product).where(Product.name == "MacBook Air M3")
+        result4 = session.execute(statement4)
+        product_4 = result4.scalar_one_or_none()
+
+        if product_4 is None:
+            raise Exception(f'Продукт не найден.')
+        else:
+            product_4.price = 1299.99
+            session.commit()
+            session.refresh(product_4)
+
+        statement5 = select(Product).where(Product.id == 3)
+        result5 = session.execute(statement5)
+        product_5 = result5.scalar_one_or_none()
+
+        if product_5 is None:
+            raise Exception(f'Продукт не найден.')
+        else:
+            session.delete(product_5)
+            session.commit()
+
+    except Exception as error:
+        print(error)
+        session.rollback()
